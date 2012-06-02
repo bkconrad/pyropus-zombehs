11efd1019c4e897cec30fd8ca43138e8af13210c
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-06-01 23:39:03 -0700

    add networking framework

--- src/zombies.js
+++ src/zombies.js
@@ -1,4 +1,56 @@
 var Zombies = (function () {
+
+  function Event (type, frame) {
+    this.type = type;
+    this.frame = frame || frameCount;
+    this.from = null;
+    this.data = null;
+  }
+
+  /**
+   * submit event to server
+   */
+  Event.prototype.submit = function () {
+    socket.emit('command', this);
+  };
+
+  /**
+   * send event to all players except originator
+   */
+  Event.prototype.relay = function () {
+    var s;
+    for (s = 0; i < io.sockets.length; i++) {
+      if (this.from != io.sockets[s].player.id)
+        io.sockets[s].emit('command', this);
+    }
+  };
+
+  /**
+   * send event to all players
+   */
+  Event.prototype.broadcast = function () {
+    io.sockets.emit('command', this);
+  };
+
+  function Player (data) {
+    data = data || {};
+    this.id = data.id || Player.id++
+    this.name = data.name || "player" + new Date().getTime();
+    this.ent = physics.create({
+      x: Math.random() * 200,
+      y: Math.random() * 200,
+      xvel: (Math.random() - .5) * 10,
+      yvel: (Math.random() - .5) * 10,
+      width: Math.random() * 20,
+      height: Math.random() * 20,
+      static: Math.random() > .5
+    });
+
+    players.push(this);
+  }
+
+  Player.id = 1;
+
     // shared
   var io
     , physics
@@ -6,6 +58,8 @@ var Zombies = (function () {
     , isServer
     , interval = 1000/50
     , startTime
+    , players
+    , eventQueue
 
     // client
     , renderer
@@ -21,6 +75,8 @@ var Zombies = (function () {
     io = _io;
     frameCount = 1;
     startTime = new Date().getTime();
+    players = [];
+    eventQueue = [];
   }
 
   function initClient (_canvas, _io, _physics, _renderer) {
@@ -33,40 +89,62 @@ var Zombies = (function () {
 
     clientConnect();
 
-    for (var i = 0; i < 20; i++) {
-      physics.create({
-        x: Math.random() * 200,
-        y: Math.random() * 200,
-        xvel: (Math.random() - .5) * 10,
-        yvel: (Math.random() - .5) * 10,
-        width: Math.random() * 20,
-        height: Math.random() * 20,
-        static: Math.random() > .5
-      });
-    }
-
-    setInterval(loop, 1000/50);
+    setInterval(loop, interval);
   }
 
   function initServer (_io, _physics) {
     initShared(_io, _physics);
     isServer = true;
     io.sockets.on('connection', serverHandler);
+
+    io.configure(function () {
+      io.set('authorization', authHandler);
+    });
+
+    setInterval(loop, interval);
+  }
+
+  function authHandler (handshakeData, callback) {
+    callback(null, true);
   }
 
   function serverHandler (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-      console.log(data);
+
+    var ev;
+
+    socket.emit('time', { frame: frameCount });
+
+    ev = new Event('join', frameCount + 10);
+    ev.broadcast();
+    eventQueue.push(ev);
+
+    socket.on('command', function () {
+      // TODO: something
     });
+
   }
 
   function setClientHandlers () {
     if(!socket)
       throw Error("Not connected");
 
-    socket.on('news', function () {
-      context.fillText("Hello!", 20, 20);
+    socket.on('time', function (data) {
+      frameCount = data.frame;
+      startTime = new Date().getTime() - interval * frameCount;
+    });
+
+    socket.on('error', function () {
+      console.log('An error has occured');
+    });
+
+    socket.on('command', function (data) {
+      console.log(data);
+      console.log(frameCount);
+      eventQueue.push(data);
+      console.log(eventQueue);
+    });
+
+    socket.on('connect', function () {
     });
   }
 
@@ -79,6 +157,7 @@ var Zombies = (function () {
     var now = new Date().getTime();
 
     while (now > startTime + interval * frameCount) {
+      checkEvents();
       physics.update(interval);
       frameCount++;
     }
@@ -87,9 +166,44 @@ var Zombies = (function () {
       renderer.render();
   }
 
+  function checkEvents () {
+    var i;
+    for (i = 0; i < eventQueue.length; i++) {
+
+      if (eventQueue[i].frame > frameCount) {
+        // wait for the event to mature
+        continue;
+      }
+
+      if (eventQueue[i].frame == frameCount) {
+        handleEvent(eventQueue[i]);
+      }
+
+      if (eventQueue[i].frame < frameCount) {
+        // old event, discard for now
+      }
+
+      // splice and decrement index
+      eventQueue.splice(i, 1);
+      i--;
+    }
+  }
+
+  function handleEvent (ev) {
+    switch (ev.type) {
+      case 'join':
+          new Player (ev.data);
+        break;
+
+      default:
+        throw Error ("Unhandled event " + ev.type);
+    }
+  }
+
   return {
     initClient: initClient,
-    initServer: initServer
+    initServer: initServer,
+    eventQueue: eventQueue
   };
 })();
 
