7936719bc1a5b417af455d644b5d78faabf6612b
Bryan Conrad <conrad.bryan.k@gmail.com>
2012-06-06 13:03:39 -0700

    add levels definitions

---
+++ src/levels.js
@@ -0,0 +1,15 @@
+var Levels = {
+  street: {
+    scenery: [
+      { model: 'tree', x: 100, y: 284 },
+      { model: 'tree', x: 200, y: 284 },
+      { model: 'tree', x: 300, y: 284 },
+      { model: 'tree', x: 400, y: 284 },
+      { model: 'tree', x: 500, y: 284 },
+      { model: 'tree', x: 600, y: 284 },
+      { model: 'tree', x: 700, y: 284 },
+    ]
+  }
+};
+
+module.exports = Levels;
